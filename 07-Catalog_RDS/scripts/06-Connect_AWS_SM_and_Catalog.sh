bf228cbdb0b6328ce80ff17836846c2670913e5f
sri <sriramrmk@gmail.com>
2026-03-18 15:34:21 -0400

    update catalog_rds info

---
+++ 07-Catalog_RDS/scripts/06-Connect_AWS_SM_and_Catalog.sh
@@ -0,0 +1,33 @@
+echo "==============================="
+echo "Set environment variables "
+echo "==============================="
+
+# Set environment variable
+export AWS_REGION="us-east-1"
+
+
+echo "==============================="
+echo "Create Secret in AWS Secrets Manager"
+echo "==============================="
+# Create Secret 
+aws secretsmanager create-secret \
+  --name catalog-db-secret-1 \
+  --region $AWS_REGION \
+  --description "MySQL credentials for Catalog microservice" \
+  --secret-string '{
+      "MYSQL_USER": "mydbadmin",
+      "MYSQL_PASSWORD": "mysqldb101"
+  }'
+
+echo "==============================="
+echo "Create the SecretProviderClass"
+echo "==============================="
+
+kubectl apply -f ../secretproviderclass/01_catalog_secretproviderclass.yaml
+
+
+echo "==============================="
+echo "Apply Catalog manifests"
+echo "==============================="
+
+kubectl apply -f ../catalog_k8s_manifests/
